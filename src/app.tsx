96d566d5bafc30ba4b8120c04a6a0bb158d08f75
sint18 <htoohtoo.slh@gmail.com>
2025-05-31 02:31:59 +0100

    Set up React, TailwindCSS, DaisyUI, and initial project structure

---
+++ src/app.tsx
@@ -0,0 +1,19 @@
+import { createRoot } from "react-dom/client";
+
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello from React!</h1>
+      <button className="btn btn-primary rounded-full">Click me</button>
+    </div>
+  );
+}
+
+const root = createRoot(document.body);
+root.render(
+  <React.StrictMode>
+    <App></App>
+  </React.StrictMode>,
+);
