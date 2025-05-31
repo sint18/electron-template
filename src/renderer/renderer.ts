f4ed1f1f0fb93fb8dc4d4c76b2f7d0f0a8d8432b
sint18 <htoohtoo.slh@gmail.com>
2025-05-31 21:35:38 +0100

    chore: refactored for better project structure and organization

--- src/renderer/renderer.ts
+++ src/renderer/renderer.ts
@@ -26,6 +26,7 @@
  * ```
  */
 
-import './index.css';
-import './app';
-console.log('👋 This message is being logged by "renderer.ts", included via Vite');
+import "./app";
+console.log(
+  '👋 This message is being logged by "renderer.ts", included via Vite',
+);
